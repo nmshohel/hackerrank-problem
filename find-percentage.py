074bad1c97f4bcd5a7740e96f2611a067bacd3b3
nmshohel <nmshohel1992@gmail.com>
2021-09-16 01:03:58 +0600

    15-9-21

--- find-percentage.py
+++ find-percentage.py
@@ -1,1 +1,10 @@
-print("hellow world")
+# a=input()
+b='Krishna 67 68 69'
+thisdict = {}
+# thisdict['color']='red';
+c=b.split(" ")
+# print(c[0])
+
+thisdict['name']=c[0]
+print(thisdict['name'])
+
